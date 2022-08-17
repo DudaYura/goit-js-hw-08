46d23bf05443583a2e87bc65cda6e49ebc7720ae
DudaYura <YurkaD@ukr.net>
2022-08-17 16:23:33 +0300

    сделал 1 и 2 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarcupRef = createGalleryMarkup(galleryItems);
+
+galleryRef.innerHTML = galleryMarcupRef;
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(item) {
+  return item
+    .map(
+      ({ preview, original, description }) =>
+        `<li><a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" 
+          alt="${description}" />
+        </a></li>`
+    )
+    .join('');
+}
 
 console.log(galleryItems);
